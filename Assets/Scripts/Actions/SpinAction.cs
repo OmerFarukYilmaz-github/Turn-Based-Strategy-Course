60af75135246062d09e676a95e5c763c4aabedd6
OmerFarukYilmaz-github <omerfarukyilmaz.iletisim@gmail.com>
2023-04-05 18:00:13 +0300

    add action ui

--- Assets/Scripts/Actions/SpinAction.cs
+++ Assets/Scripts/Actions/SpinAction.cs
@@ -27,7 +27,7 @@ public class SpinAction : BaseAction
         }
     }
 
-    public void Spin(Action onActionComplete)
+    public override void TakeAction(GridPosition gridPosition, Action onActionComplete)
     {
         this.onActionComplete = onActionComplete;
         isActive = true;
@@ -35,4 +35,19 @@ public class SpinAction : BaseAction
     }
 
 
+    public override string GetActionName()
+    {
+        return "Spin";
+    }
+
+    public override List<GridPosition> GetValidActionGridPositionList()
+    {
+        GridPosition unitGridPosition = unit.GetGridPosition();
+
+        return new List<GridPosition>
+        {
+            unitGridPosition
+        };
+    }
+    
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,11 +8,13 @@ public class Unit : MonoBehaviour
     private GridPosition gridPosition;
     private MoveAction moveAction;
     private SpinAction spinAction;
+    private BaseAction[] baseActionArray;
 
     private void Awake()
     {
         moveAction = GetComponent<MoveAction>();
         spinAction = GetComponent<SpinAction>();
+        baseActionArray = GetComponents<BaseAction>();
     }
 
     private void Start()
@@ -47,5 +49,9 @@ public class Unit : MonoBehaviour
         return gridPosition;
     }
 
+    public BaseAction[] GetBaseActionArray()
+    {
+        return baseActionArray;
+    }
 
 }
