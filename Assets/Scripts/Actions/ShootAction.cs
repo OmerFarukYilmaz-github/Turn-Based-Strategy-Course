6eeada2cafa4e3f5616622d6ce3831c9101308ef
OmerFarukYilmaz-github <omerfarukyilmaz.iletisim@gmail.com>
2023-04-07 16:47:34 +0300

    add basic enemy ai

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -148,8 +148,6 @@ public class ShootAction : BaseAction
 
     public override void TakeAction(GridPosition gridPosition, Action onActionComplete)
     {
-        ActionStart(onActionComplete);
-
         targetUnit = LevelGrid.Instance.GetUnitAtGridPosition(gridPosition);
 
         state = State.Aiming;
@@ -157,5 +155,18 @@ public class ShootAction : BaseAction
         stateTimer = aimingStateTime;
 
         canShootBullet = true;
+
+        ActionStart(onActionComplete);
     }
+
+    public Unit GetTargetUnit()
+    {
+        return targetUnit;
+    }
+
+    public int GetMaxShootDistance()
+    {
+        return maxShootDistance;
+    }
+
 }
--- Assets/Scripts/Actions/SpinAction.cs
+++ Assets/Scripts/Actions/SpinAction.cs
@@ -28,9 +28,9 @@ public class SpinAction : BaseAction
 
     public override void TakeAction(GridPosition gridPosition, Action onActionComplete)
     {
-        ActionStart(onActionComplete);
-
         totalSpinAmount = 0f;
+
+        ActionStart(onActionComplete);
     }
 
 
@@ -51,7 +51,7 @@ public class SpinAction : BaseAction
 
     public override int GetActionPointsCost()
     {
-        return 2;
+        return 1;
     }
     
 }
